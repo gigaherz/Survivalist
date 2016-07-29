7ae744186cb1f64ef9c8ef53237c9ed3abd43ad3
gigaherz <gigaherz@gmail.com>
2016-07-30 00:47:30 +0200

    Avoid crash if JEI was installed but the drying rack or chopping block were disabled. Also disable integration for the respective recipes if the blocks are disabled.

--- src/main/java/gigaherz/survivalist/integration/JEIPlugin.java
+++ src/main/java/gigaherz/survivalist/integration/JEIPlugin.java
@@ -1,5 +1,6 @@
 package gigaherz.survivalist.integration;
 
+import gigaherz.survivalist.ConfigManager;
 import gigaherz.survivalist.Survivalist;
 import gigaherz.survivalist.integration.analyzer.ChoppingCategory;
 import gigaherz.survivalist.integration.analyzer.ChoppingRecipeHandler;
@@ -20,21 +21,29 @@ public class JEIPlugin implements IModPlugin
     @Override
     public void register(@Nonnull IModRegistry registry)
     {
-        registry.addRecipeCategories(
-                new ChoppingCategory(registry.getJeiHelpers().getGuiHelper()),
-                new DryingCategory(registry.getJeiHelpers().getGuiHelper()));
+        if (ConfigManager.instance.enableDryingRack)
+        {
+            registry.addRecipeCategories(new DryingCategory(registry.getJeiHelpers().getGuiHelper()));
+            registry.addRecipeHandlers(new DryingRecipeHandler());
+            registry.addRecipes(DryingRecipeWrapper.getRecipes());
+            if (Survivalist.rack != null)
+            {
+                registry.addRecipeCategoryCraftingItem(new ItemStack(Survivalist.rack), DryingCategory.UID);
+            }
+        }
 
-        registry.addRecipeHandlers(
-                new ChoppingRecipeHandler(),
-                new DryingRecipeHandler());
-
-        registry.addRecipeCategoryCraftingItem(new ItemStack(Survivalist.chopping_block), ChoppingCategory.UID);
-        registry.addRecipeCategoryCraftingItem(new ItemStack(Survivalist.rack), DryingCategory.UID);
+        if (ConfigManager.instance.enableChopping)
+        {
+            registry.addRecipeCategories(new ChoppingCategory(registry.getJeiHelpers().getGuiHelper()));
+            registry.addRecipeHandlers(new ChoppingRecipeHandler());
+            registry.addRecipes(ChoppingRecipeWrapper.getRecipes());
+            if (Survivalist.chopping_block != null)
+            {
+                registry.addRecipeCategoryCraftingItem(new ItemStack(Survivalist.chopping_block), ChoppingCategory.UID);
+            }
+        }
 
         //registry.getRecipeTransferRegistry().addRecipeTransferHandler(new EssentializerCategory.TransferInfo());
-
-        registry.addRecipes(ChoppingRecipeWrapper.getRecipes());
-        registry.addRecipes(DryingRecipeWrapper.getRecipes());
     }
 
     @Override
