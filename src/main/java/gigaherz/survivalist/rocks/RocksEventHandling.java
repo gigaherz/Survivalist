36a8f4bdbdfb7f475b3ced7e23d6fce9798d9e20
gigaherz <gigaherz@gmail.com>
2016-08-24 16:17:54 +0200

    Avoid crashing if a block's drops are in an ImmutableList. Replacing drops will not be possible for those blocks, and I consider them to break the "design contract" for that method.

--- src/main/java/gigaherz/survivalist/rocks/RocksEventHandling.java
+++ src/main/java/gigaherz/survivalist/rocks/RocksEventHandling.java
@@ -1,5 +1,6 @@
 package gigaherz.survivalist.rocks;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import gigaherz.survivalist.ConfigManager;
 import gigaherz.survivalist.Survivalist;
@@ -25,75 +26,97 @@ public class RocksEventHandling
     @SubscribeEvent
     public void onHarvestBlock(BlockEvent.HarvestDropsEvent ev)
     {
-        if (!ev.isSilkTouching())
-        {
-            List<ItemStack> newDrops = Lists.newArrayList();
+        if (ev.isSilkTouching())
+            return;
 
-            for (ItemStack drop : ev.getDrops())
+        List<ItemStack> drops = ev.getDrops();
+        if (drops instanceof ImmutableList)
+        {
+            Survivalist.logger.warn("WARNING: Some mod is returning an ImmutableList from HarvestBlocks, replacing drops will NOT be possible.");
+            return;
+        }
+
+        boolean anyChanged = false;
+        List<ItemStack> newDrops = Lists.newArrayList();
+
+        for (ItemStack drop : drops)
+        {
+            if (drop.getItem() == Item.getItemFromBlock(Blocks.COBBLESTONE) && ConfigManager.instance.replaceStoneDrops)
             {
-                if (drop.getItem() == Item.getItemFromBlock(Blocks.COBBLESTONE) && ConfigManager.instance.replaceStoneDrops)
+                newDrops.add(new ItemStack(Survivalist.rock, 4));
+                anyChanged = true;
+            }
+            else if (drop.getItem() == Item.getItemFromBlock(Blocks.STONE) && ConfigManager.instance.replaceStoneDrops)
+            {
+                switch (drop.getMetadata())
                 {
-                    newDrops.add(new ItemStack(Survivalist.rock, 4));
-                }
-                else if (drop.getItem() == Item.getItemFromBlock(Blocks.STONE) && ConfigManager.instance.replaceStoneDrops)
-                {
-                    switch (drop.getMetadata())
-                    {
-                        case 0:
-                            newDrops.add(new ItemStack(Survivalist.rock, 4, 0));
-                            break;
-                        case 5:
-                            newDrops.add(new ItemStack(Survivalist.rock, 4, 1));
-                            break;
-                        case 3:
-                            newDrops.add(new ItemStack(Survivalist.rock, 4, 2));
-                            break;
-                        case 1:
-                            newDrops.add(new ItemStack(Survivalist.rock, 4, 3));
-                            break;
-                        default:
-                            newDrops.add(drop);
-                    }
-                }
-                else if (drop.getItem() == Item.getItemFromBlock(Blocks.IRON_ORE) && ConfigManager.instance.replaceIronOreDrops)
-                {
-                    newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 0));
-                }
-                else if (drop.getItem() == Item.getItemFromBlock(Blocks.GOLD_ORE) && ConfigManager.instance.replaceGoldOreDrops)
-                {
-                    newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 1));
-                }
-                else if (ConfigManager.instance.replaceModOreDrops)
-                {
-                    if (Survivalist.hasOreName(drop, "oreCopper"))
-                    {
-                        newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 2));
-                    }
-                    else if (Survivalist.hasOreName(drop, "oreTin"))
-                    {
-                        newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 3));
-                    }
-                    else if (Survivalist.hasOreName(drop, "oreLead"))
-                    {
-                        newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 4));
-                    }
-                    else if (Survivalist.hasOreName(drop, "oreSilver"))
-                    {
-                        newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 5));
-                    }
-                    else
-                    {
+                    case 0:
+                        newDrops.add(new ItemStack(Survivalist.rock, 4, 0));
+                        anyChanged = true;
+                        break;
+                    case 5:
+                        newDrops.add(new ItemStack(Survivalist.rock, 4, 1));
+                        anyChanged = true;
+                        break;
+                    case 3:
+                        newDrops.add(new ItemStack(Survivalist.rock, 4, 2));
+                        anyChanged = true;
+                        break;
+                    case 1:
+                        newDrops.add(new ItemStack(Survivalist.rock, 4, 3));
+                        anyChanged = true;
+                        break;
+                    default:
                         newDrops.add(drop);
-                    }
+                }
+            }
+            else if (drop.getItem() == Item.getItemFromBlock(Blocks.IRON_ORE) && ConfigManager.instance.replaceIronOreDrops)
+            {
+                newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 0));
+                anyChanged = true;
+            }
+            else if (drop.getItem() == Item.getItemFromBlock(Blocks.GOLD_ORE) && ConfigManager.instance.replaceGoldOreDrops)
+            {
+                newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 1));
+                anyChanged = true;
+            }
+            else if (ConfigManager.instance.replaceModOreDrops)
+            {
+                if (Survivalist.hasOreName(drop, "oreCopper"))
+                {
+                    newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 2));
+                    anyChanged = true;
+                }
+                else if (Survivalist.hasOreName(drop, "oreTin"))
+                {
+                    newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 3));
+                    anyChanged = true;
+                }
+                else if (Survivalist.hasOreName(drop, "oreLead"))
+                {
+                    newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 4));
+                    anyChanged = true;
+                }
+                else if (Survivalist.hasOreName(drop, "oreSilver"))
+                {
+                    newDrops.add(new ItemStack(Survivalist.rock_ore, 2 + Math.round(2 * rnd.nextFloat()), 5));
+                    anyChanged = true;
                 }
                 else
                 {
                     newDrops.add(drop);
                 }
             }
+            else
+            {
+                newDrops.add(drop);
+            }
+        }
 
-            ev.getDrops().clear();
-            ev.getDrops().addAll(newDrops);
+        if (anyChanged)
+        {
+            drops.clear();
+            drops.addAll(newDrops);
         }
     }
 }
